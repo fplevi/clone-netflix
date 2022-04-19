261f5bb183eeefe731362e1b0f3dca1a2abade83
fplevi <fplevi@gmail.com>
2022-04-19 17:05:28 -0300

    Add arquivos clone netflix

---
+++ js/owl/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:false,
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:5
+            },
+            1000:{
+                items:8
+            }
+        }
+    })
+  });
